88a6aa29a4e6b473ca9dfb9fa0611f10db3fc177
baywet <vincentbiret@hotmail.com>
2014-07-23 00:58:31 -0400

    Adding Javascript + test page

---
+++ LAYOUTS/JSSample/Scripts/sample.js
@@ -0,0 +1,31 @@
+﻿'use strict';
+var Sample = Sample || {};
+Sample.Global = Sample.Global || {};
+Sample.Global.Resources = null;
+
+Sample.Global.LoadResources = function () {
+    var context = new SP.ClientContext.get_current();
+    this.website = context.get_web();
+    context.load(this.website, 'Title', 'ServerRelativeUrl');
+    context.executeQueryAsync(Function.createDelegate(this, Sample.Global.LoadUserInfoSuccess), Function.createDelegate(this, Sample.Global.LoadUserInfoFailed));
+
+}
+Sample.Global.LoadUserInfoSuccess = function (sender, args) {
+    var locLang = (navigator.language) ? navigator.language : navigator.userLanguage;
+    var serverRelativeUrl = this.website.get_serverRelativeUrl();
+    if (serverRelativeUrl.length < 2) { serverRelativeUrl = ""; }
+    var url = serverRelativeUrl + "/_layouts/ScriptResx.ashx?culture=" + locLang + "&name=MyResource";
+    sp$.getScript(url, function (script, status) {
+        Sample.Global.Resources = Res;
+        Res = null;
+    });
+    this.website = null;
+}
+Sample.Global.LoadUserInfoFailed = function (sender, args) {
+    //should do something?
+}
+
+Sample.Global.LoadResources();
+
+//rest of you methods and objects for your solution...
+alert(Sample.Global.Resources.helloWorld);//caution first letter of the resources key will always be in lowercase : json
---
+++ LAYOUTS/JSSample/test.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace JSLocalizeSample.LAYOUTS.JSSample
+{
+    public partial class test : LayoutsPageBase
+    {
+        protected override void OnPreRender(EventArgs e)
+        {
+            //SPRibbon currentRibbon = SPRibbon.GetCurrent(this.Page);
+            //if (currentRibbon != null)
+            //{
+                ScriptLink.RegisterScriptAfterUI(this.Page, "SP.Core.js", false, true);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "CUI.js", false, true);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "core.js", true, false);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "SP.Ribbon.js", false, true);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "SP.Runtime.js", false, true);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "SP.js", false, true);
+
+                ScriptLink.RegisterScriptAfterUI(this.Page, "JSample/Scripts/jquery.min.js", false, true);
+                ScriptLink.RegisterScriptAfterUI(this.Page, "JSample/Scripts/sample.js", false, true);
+            //}
+            base.OnPreRender(e);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
